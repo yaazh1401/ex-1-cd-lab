841fb399d9abfa3078a8f59893295d05253a414d
Yaazhini S <yaazhinisekar7@gmail.com>
2026-05-11 10:38:53 +0530

    Add files via upload

---
+++ exp10308.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_EXPRESSION_SIZE 100
+
+int main() {
+	int i = 0, j = 0, x = 0, n, flag = 0;
+	int k;
+	char b[MAX_EXPRESSION_SIZE], d[15], c, srch;
+
+	printf("Enter the Expression terminated by $: ");
+	while ((c = getchar()) != '$' && i < MAX_EXPRESSION_SIZE - 1) {
+		b[i++] = c;
+	}
+	b[i] = '\0';
+	n = i - 1;
+
+	printf("\nGiven Expression: %s\n", b);
+
+	printf("\nSymbol Table\n");
+	printf("Symbol\tType\t\tAddress\n");
+
+	for (j = 0; j <= n; j++) {
+		c = b[j];
+		if (isalpha((unsigned char)c)) {
+			int alreadyExists = 0;
+			for (k = 0; k < x; k++) {
+				if (d[k] == c) {
+					alreadyExists = 1;
+					break;
+				}
+			}
+
+			if (!alreadyExists) {
+				d[x] = c;
+				printf("%c\tidentifier\t%p\n", c, (void*)&d[x]);
+				x++;
+			}
+		}
+	}
+
+	// Clear input buffer
+	while ((c = getchar()) != '\n' && c != EOF);
+
+	printf("\nEnter the symbol to search: ");
+	srch = getchar();
+
+	for (i = 0; i < x; i++) {
+		if (srch == d[i]) {
+			printf("Symbol Found\n");
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0)
+		printf("Symbol Not Found\n");
+
+	return 0;
+}
